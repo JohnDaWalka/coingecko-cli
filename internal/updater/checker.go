80f300b33aa003883f773e164ba0b550ca3516be
irisyann <irisyan716@gmail.com>
2026-04-21 16:34:58 +0800

    Fix lint error

--- internal/updater/checker.go
+++ internal/updater/checker.go
@@ -81,7 +81,7 @@ func fetchLatest(ctx context.Context) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	if resp.StatusCode != http.StatusOK {
 		return "", fmt.Errorf("GitHub API returned %d", resp.StatusCode)
